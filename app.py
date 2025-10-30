c651ea28140ff9bc7e301f9babf9b7c1911331c7
Amanda <amanda2377ramos@gmail.com>
2025-10-30 19:25:25 -0300

    Configure app to use environment port variable
    Update app to run on specified port from environment variable

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from routes.usuario import usuario_routes
 from routes.contas import contas_routes
 from routes.metas import metas_routes
 from routes.relatorio import relatorio_routes
+import os  
 
 app = Flask(__name__)
 CORS(app)
@@ -18,4 +19,5 @@ for rule in app.url_map.iter_rules():
     print(rule)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host="0.0.0.0", port=port, debug=True) 
