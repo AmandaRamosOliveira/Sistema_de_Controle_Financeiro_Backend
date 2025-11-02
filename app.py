b60a6ba24023db7c6c30e3d33f01405f4a9bffab
Amanda <amanda2377ramos@gmail.com>
2025-11-02 12:47:31 -0300

    Integrate dotenv for environment variable loading
    Added dotenv support to load environment variables.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 from flask import Flask
 from flask_cors import CORS
 from routes.usuario import usuario_routes
 from routes.contas import contas_routes
 from routes.metas import metas_routes
 from routes.relatorio import relatorio_routes
-import os  
 
 app = Flask(__name__)
 CORS(app)
@@ -14,10 +18,10 @@ app.register_blueprint(contas_routes, url_prefix='/api')
 app.register_blueprint(metas_routes, url_prefix='/api')
 app.register_blueprint(relatorio_routes, url_prefix='/api')
 
-
 for rule in app.url_map.iter_rules():
     print(rule)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(host="0.0.0.0", port=port, debug=True) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
