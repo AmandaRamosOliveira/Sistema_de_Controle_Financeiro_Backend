4d4707dcb79c8a30ced603e8ef6404ef7a39193e
Amanda <amanda2377ramos@gmail.com>
2025-11-05 09:08:25 -0300

    Restrict CORS to specific origin

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from routes.metas import metas_routes
 from routes.relatorio import relatorio_routes
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://sistema-de-controle-financeiro-fron.vercel.app"])
 
 app.register_blueprint(usuario_routes, url_prefix="/api")
 app.register_blueprint(contas_routes, url_prefix='/api')
