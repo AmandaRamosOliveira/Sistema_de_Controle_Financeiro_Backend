f6b1cc031aa3ab3a40b6690639b2a63c4015b565
Amanda <amanda2377ramos@gmail.com>
2025-11-05 10:08:51 -0300

    Change default port from 5000 to 8080

--- app.py
+++ app.py
@@ -44,5 +44,6 @@ for rule in app.url_map.iter_rules():
     print(rule)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+
